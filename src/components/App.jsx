24e357a07de6eac2551f0aee3d2d8e42652f3ae7
Artur Levchenko <99216307+LightForLife@users.noreply.github.com>
2023-08-04 17:36:13 +0300

    add banner component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import Navbar from './Navbar/Navbar';
+import Banner from './Banner/Banner';
+import Features from './Features/Features';
 
 export const App = () => {
   return (
     <div className="w-full h-auto bg-bodyColor text-lightText">
       <div className="max-w-screen-2xl mx-auto px-16">
         <Navbar />
+        <Banner />
+        <Features />
       </div>
     </div>
   );
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,13 +1,35 @@
 import React from 'react';
+import { Link } from 'react-scroll';
 import { logo } from '../../assets/index';
+import { navLinksData } from 'constants';
 
 const Navbar = () => {
   return (
-    <div>
+    <div className="w-full h-24 mx-auto flex justify-between items-center font-titleFont border-b-[1px] border-b-gray-600">
       <div>
-        <img src={logo} alt="logo" className="w-12 h-auto" />
+        <img src={logo} alt="logo" className="w-16 h-auto" />
       </div>
-      <div></div>
+      <div>
+        <ul className="flex items-center gap-10">
+          {navLinksData.map(({ _id, title, link }) => (
+            <li
+              className="text-base font-normal text-gray-400 tracking-wide cursor-pointer hover:text-designColor duration-300"
+              key={_id}
+            >
+              <Link
+                activeClass="active"
+                to={link}
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+              >
+                {title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
